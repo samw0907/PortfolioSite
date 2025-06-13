790d8fd640b6c542f57202e2735085d808fbb4d1
Sam <swilliamson_0907@outlook.com>
2025-06-13 09:53:14 +0300

    spline mobile fix

--- portfolio-frontend/src/components/Spline3D.tsx
+++ portfolio-frontend/src/components/Spline3D.tsx
@@ -3,7 +3,7 @@ import Spline from '@splinetool/react-spline';
 const Spline3D = () => {
   return (
     <div
-      className="w-full h-[300px] sm:h-[400px] relative z-10 touch-none select-none"
+      className="w-full h-[300px] sm:h-[400px] relative z-10"
       style={{ WebkitTapHighlightColor: 'transparent' }}
     >
       <Spline scene="https://prod.spline.design/D2CC6Aw37W8fxD2t/scene.splinecode" />
