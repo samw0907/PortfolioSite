242cc45879d137d76ce067a14047fcc55a7d67bb
Sam Williamson <swilliamson_0907@outlook.com>
2026-01-07 13:17:26 +0200

    minor config changes

--- portfolio-frontend/src/vite-env.d.ts
+++ portfolio-frontend/src/vite-env.d.ts
@@ -1,1 +1,20 @@
 /// <reference types="vite/client" />
+/// <reference types="vite/client" />
+
+declare module "*.css";
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
